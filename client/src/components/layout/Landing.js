633ca7d2d29588a87b4e2ef030ec90518f6cd058
RyanPPitts <ryanpitts26@gmail.com>
2019-11-13 23:00:04 -0800

    adding redux to manage the state of the components
    This is not necessary for a simple form but will be necessary for a larger MERN application

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Get your dream job</b> learn from experienced developers in your local market {" "}
+              <span style={{ fontFamily: "monospace" }}>CODER'S EDGE</span> helps you connect with professionalsss
+              that will help you get your next high paying tech. job.
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              Get advice on technical interviews, resumes, portfolio from experienced, industry leading sofware engineers
+              in your area.
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Landing;
